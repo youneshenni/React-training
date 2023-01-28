b66a286d437ef50106bf32476c7cf79291023882
younes <youn.henni@gmail.com>
2023-01-28 14:46:57 +0100

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,68 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React, { useState } from "react";
-
-let data = [
-  { id: 1, name: "John Doe", age: 42 },
-  { id: 2, name: "Jane Doe", age: 39 },
+const defaultData = [
+  { name: "Name", email: "Email", phone: "Phone" },
+  { name: "Name2", email: "Email2", phone: "Phone2" },
 ];
+
 function App() {
-  const [first, setFirst] = useState(data);
+  const [data, setData] = useState(defaultData);
+  const [showForm, setShowForm] = useState(false);
+  const [update, setUpdate] = useState(null);
+  console.log(data);
   return (
-    <ul>
-      {first.map((person) => (
-        <li key={person.id}>
-          {person.name} is {person.age} years old.
-          <button>Edit</button>
-          <button
-            onClick={() => {
-              setFirst(first.filter((p) => p.id !== person.id));
-            }}
-          >
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {data.map(({ name, email, phone }) => (
+          <li>
+            {name} {email} {phone}
+            <br />
+            <button
+              onClick={() => {
+                setData(data.filter((item) => item.name !== name));
+                console.log(data);
+              }}
+            >
+              Supprimer
+            </button>
+            <button onClick={() => setUpdate(email)}>Modifier</button>
+            <form style={{ display: update === email ? "block" : "none" }}>
+              <label>Name</label>
+              <input type="text" name="name" />
+              <br />
+              <label>Email</label>
+              <input type="text" name="email" />
+              <br />
+            </form>
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          setShowForm(true);
+        }}
+      >
+        +
+      </button>
+      <form
+        style={{
+          display: showForm ? "block" : "none",
+        }}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label>Name</label>
+        <input type="text" name="name" />
+        <br />
+        <label>Email</label>
+        <input type="text" name="email" />
+        <br />
+        <label>Phone</label>
+        <input type="text" name="phone" />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
 
